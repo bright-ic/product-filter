de928b82176ff9efef45a358c2585415475a37f4
Bright Onwukwe <onwukweb@gmail.com>
2020-04-06 08:47:59 +0100

    Initial setup for client and server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+const DBConnection = process.env.MONGODB_URI || `mongodb://localhost:27017/product-filter`;
+const db = mongoose.connect(DBConnection, { useUnifiedTopology: true, useNewUrlParser: true });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
